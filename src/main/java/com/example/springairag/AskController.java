7ecc17328201795e2acd793360c78b60204e6b58
wanting hsieh <Wanting.Hsieh@cox.com>
2024-04-18 09:47:24 -0400

    initial commit

---
+++ src/main/java/com/example/springairag/AskController.java
@@ -0,0 +1,56 @@
+package com.example.springairag;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ask")
+public class AskController {
+
+    private final ChatClient aiClient;
+    private final VectorStore vectorStore;
+
+    @Value("classpath:/rag-prompt-template.st")
+    private Resource ragPromptTemplate;
+
+
+    public AskController(ChatClient aiClient, VectorStore vectorStore) {
+        this.aiClient = aiClient;
+        this.vectorStore = vectorStore;
+    }
+
+    @PostMapping
+    public Answer ask(@RequestBody Question question) {
+    	try {
+    		SSLUtil.turnOffSslChecking();
+    	}catch(Exception e) {
+    		System.out.println("exception SSLUtil.turnOffSslChecking()");
+    	}
+        List<Document> similarDocuments = vectorStore.similaritySearch(SearchRequest.query(question.question()).withTopK(2));
+        List<String> contentList = similarDocuments.stream().map(Document::getContent).toList();
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+        Map<String, Object> promptParameters = new HashMap<>();
+        promptParameters.put("input", question.question());
+        promptParameters.put("documents", String.join("\n", contentList));
+        Prompt prompt = promptTemplate.create(promptParameters);
+
+        ChatResponse response = aiClient.call(prompt);
+        return new Answer(response.getResult().getOutput().getContent());
+    }
+
+}
